838723a5a6c4db9b1a9cb3ec87f31274edf46a29
Angel Ni¤o C. Ortega <ortegacanillo76@gmail.com>
2023-11-04 09:47:20 +0800

    update

--- src/App.ts
+++ src/App.ts
@@ -24,31 +24,57 @@ export class App {
         const publicKey = 'BCol311jRW4M59BwcFAMiESdjaTHaNGQTJ-kC88feFnLEJ6nC-2JFOBcMX-rLRIO8NaaXYwDRCLn1a_s4XgR384';
         const privateKey = 'CjLPPZaLJNhv6dynvL_BMURqHwWRpjfI-K2G0PZkXB0';
 
-        const sub = {
-            endpoint: 'https://updates.push.services.mozilla.com/wpush/v2/gAAAAABlQ33HwyGckJgFadvDbKdT_ridScZnUJ-0J8gdzu3cWT9-JZyGVdw1wyTnLLS1BhxfMPkiXWSh9ziwMxY7AgWrEuEPl5BRjDKy4s2cg2IaVgyjP5PXuzz5ZaPhCPkW02WDroTCun5mFmZk9PIRRQ6JFlYi-4eIY2tsEiUHbCAJwAQ7Te4',
-            expirationTime: null,
-            keys: {
-                p256dh: 'BBfElKxsq0AQbwSGQvjo8-9JH7qD1Hgy7ompty4IUp35RamJPt-yFL7tuMncMcaEso3PE8Fwq8g0L3t1of07Xe0',
-                auth: 'd6MTOjqx4r1s-HN5b3PWkw',
-            },
-        };
+        // this.app.get("/", (req, res, next) => {
+        //     const sub = {
+        //         endpoint: 'https://updates.push.services.mozilla.com/wpush/v2/gAAAAABlRLA-6qjCm2sltTF0ya5ZIeDVWGEt4r04tES0UCzxEgCRRW_kBIdosWknxhLftjjFs5DQLsHkcaZqi7TritWB2NjLN--nqrkCxGKpUGk3Fdurzzs8jJhEXW2s6UoDU1nk34xHPcV0ic44jg6DJ2xiMkg-yIuKqHtR7GkoESLHl6Qjgew',
+        //         expirationTime: null,
+        //         keys: {
+        //             p256dh: 'BEZSRZbvIHBulrfRoS6Mvb6it-k4ELpxvrHHujTXVHHVMy2i20p_6zamzxd4pvoFZ-Wy7Ps3UFTlO4L3LNPWRwM',
+        //             auth: '0c-elgA3RQhTekxlByXkyQ',
+        //         },
+        //     };
+    
+        //     const payLoad = {
+        //         notification: {
+        //             data: { url: 'http://www.youtube.com' },
+        //             title: 'Test Notification from Server',
+        //             vibrate: [100, 50, 100]
+        //         },
+        //     };
+    
+        //     webPush.setVapidDetails('mailto:sample@mail.com', publicKey, privateKey);
+    
+        //     webPush.sendNotification(sub, JSON.stringify(payLoad))
+        //     .then((result) => {
+        //         console.log(result);
+        //     })
+        //     .catch((err) => {
+        //         console.log(err);
+        //     });
+        // });
 
-        const payLoad = {
-            notification: {
-                data: { url: 'http://www.youtube.com/funofheuristic' },
-                title: 'Test Notification from Server',
-                vibrate: [100, 50, 100]
-            },
-        };
-
-        webPush.setVapidDetails('mailto:sample@mail.com', publicKey, privateKey);
-
-        webPush.sendNotification(sub, JSON.stringify(payLoad))
-        .then((result) => {
-            console.log(result);
-        })
-        .catch((err) => {
-            console.log(err);
+        this.app.post("/subscribe", (req, res, next) => {
+            const subscription = req.body;
+            console.log(subscription);
+    
+            const payLoad = {
+                notification: {
+                    data: { url: 'http://www.youtube.com' },
+                    title: 'Test Notification from Server',
+                    vibrate: [100, 50, 100]
+                },
+            };
+    
+            webPush.setVapidDetails('mailto:sample@mail.com', publicKey, privateKey);
+    
+            webPush.sendNotification(subscription, JSON.stringify(payLoad))
+            .then((result) => {
+                console.log(result);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+            res.status(201).json("Subscription successful");
         });
 
         this.app.post("/data", (req, res, next) => {
@@ -58,7 +84,6 @@ export class App {
             res.send(userData);
         });
 
-        this.app.get("/", (req, res, next) => res.send("App Index page"));
 
         this.app.use("/api/push-notification", this.pushNotification.getRouter);
     }
